cb3a718aec7781532ff7d62007ae0c0fbdacd286
yoonsii <118307793+yoonsii@users.noreply.github.com>
2024-06-09 23:14:42 +1200

    Update provisionubsx.sh
    Working active-passive NXC failover with AZ

--- HA/ActivePassive/provisionubsx.sh
+++ HA/ActivePassive/provisionubsx.sh
@@ -1,5 +1,7 @@
 
       # update app repository
+    yes | sudo ufw enable 
+
       echo "update app repository"
 
       apt-get update
@@ -102,7 +104,7 @@ echo $POSITION_VARIABLE > /tmp/position.txt
 
 #Connect to MySQL
 sudo mysql << EOF
-CREATE USER 'yoonsi'@'localhost' IDENTIFIED BY '********';
+CREATE USER 'yoonsi'@'localhost' IDENTIFIED BY 'Deathscythe1';
 CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON nextcloud.* TO 'yoonsi'@'localhost';
 FLUSH PRIVILEGES;
@@ -131,12 +133,34 @@ EOF
 # sudo mysql -u root -e "SHOW MASTER STATUS;" | tee /tmp/masterstatus.log
 # sudo mysql -u root -e "UNLOCK TABLES;"
 
+
+sudo apt install cifs-utils -y #required for mount
+sudo mkdir /mnt/nextcloud
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/20240608mystorage.cred" ]; then
+    sudo bash -c 'echo "username=20240608mystorage" >> /etc/smbcredentials/20240608mystorage.cred'
+    sudo bash -c 'echo "password=kba3Wy/maxnVPLFAiXiSBxARVmxv6M0URt7F8YKaUI3NECHPJVY7uDinwKvc8+5T2ptbaIeWFgtU+ASt5tAu5A==" >> /etc/smbcredentials/20240608mystorage.cred'
+fi
+sudo chmod 600 /etc/smbcredentials/20240608mystorage.cred
+
+sudo bash -c 'echo "//20240608mystorage.file.core.windows.net/20240608fileshare /mnt/nextcloud cifs nofail,credentials=/etc/smbcredentials/20240608mystorage.cred,uid=33,gid=33,dir_mode=0750,file_mode=0750,serverino,nosharesock,actimeo=30" >> /etc/fstab'
+sudo mount -t cifs //20240608mystorage.file.core.windows.net/20240608fileshare /mnt/nextcloud -o credentials=/etc/smbcredentials/20240608mystorage.cred,uid=33,gid=33,dir_mode=0750,file_mode=0750,serverino,nosharesock,actimeo=30
+      sudo chown -R www-data: /mnt/nextcloud
+      sudo chmod -R 750 /mnt/nextcloud
+
+
+
+
 cd /var/www/html/nextcloud/
 
 sudo -u www-data php occ  maintenance:install \
 --database='mysql' --database-name='nextcloud' \
 --database-user='yoonsi' --database-pass='Deathscythe1' \
 --admin-user='admin' --admin-pass='password'
+
+sudo rsync -avz /var/www/html/nextcloud/data/ /mnt/nextcloud/
 
 cd /var/www/html/nextcloud/config/
 
@@ -149,6 +173,21 @@ sed '/0 => '"'localhost'"',/a\
 
 # Replace the original file with the modified version
 mv "$tmp_file" config.php
+
+# Create a temporary file
+tmp_file=$(mktemp)
+
+# Insert the new line after the 'localhost' line
+sed '/1 => '"'192.168.56.50'"',/a\
+    2 => '"'192.168.56.100'"',' config.php > "$tmp_file"
+
+# Replace the original file with the modified version
+mv "$tmp_file" config.php
+
+config_file="/var/www/html/nextcloud/config/config.php"
+new_value="'datadirectory' => '/mnt/nextcloud'"
+
+    sed -i "s|'datadirectory' => '.*'|$new_value|" "$config_file"
 
 
 	chown -R www-data: /var/www/html/nextcloud/
@@ -163,10 +202,115 @@ systemctl restart sshd
   sleep 15
 
 
+
+sudo ufw enable
+sudo ufw allow 5405/udp
+sudo ufw allow 22/tcp 
+sudo ufw allow 3306/tcp
+sudo ufw allow 3306/udp
+sudo ufw allow 80/tcp
+
+
+#### Add corosync stuff to end of both provisions
+
+
+
+# might be the case that I don't need to do any different steps on the second server. Might be able to run this on both. 
+# try when possible.
+
+# install corosync & pacemaker - make sure you understand exactly what these two do
+sudo apt install -y corosync pacemaker pcs
+
+# update this to do a test                  TODO
+
+sudo rm -f /etc/corosync/corosync.conf
+
+sudo cat << EOF > /etc/corosync/corosync.conf
+totem {
+    version: 2
+    secauth: off
+    cluster_name: mycluster
+    transport: udpu
+    interface {
+        ringnumber: 0
+        bindnetaddr: 192.168.56.0
+        mcastport: 5405
+    }
+}
+
+logging {
+    fileline: off
+    to_stderr: yes
+    to_logfile: yes
+    logfile: /var/log/corosync/corosync.log
+    to_syslog: yes
+    debug: off
+    timestamp: on
+    logger_subsys {
+        subsys: AMF
+        debug: off
+        tags: enter|leave|trace1|trace2|trace3|trace4|trace6
+    }
+}
+
+nodelist {
+    node {
+        ring0_addr: 192.168.56.50
+        nodeid: 1
+        name: ubsx
+    }
+
+    node {
+        ring0_addr: 192.168.56.51
+        nodeid: 2
+        name: ubsx2
+    }
+}
+
+quorum {
+    provider: corosync_votequorum
+    two_node: 1
+}
+
+EOF
+
+sudo echo "192.168.56.51     ubsx2" >> /etc/hosts
+
 ip link set enp0s8 down
-
 ip addr flush dev enp0s8
-
 ip addr add 192.168.56.50/24 dev enp0s8
-
 ip link set enp0s8 up
+
+sudo chmod 755 /etc/corosync/corosync.conf 
+
+#some stuff to do here on ubsx2 - complete after - actually not?
+
+sudo systemctl start corosync
+sudo systemctl enable corosync
+sudo systemctl start pacemaker
+sudo systemctl enable pacemaker
+
+sudo systemctl status corosync
+sudo systemctl status pacemaker
+
+sudo pcs property set stonith-enabled=false
+sudo pcs property set no-quorum-policy=ignore
+sudo pcs property set migration-threshold=1
+#https://clusterlabs.org/quickstart-ubuntu.html
+
+sudo pcs resource create virtual_ip ocf:heartbeat:IPaddr2 ip=192.168.56.100 cidr_netmask=24 nic=enp0s8 op monitor interval=30s
+
+	  
+
+sudo crm_mon -1
+
+
+sudo systemctl restart corosync
+sudo systemctl restart pacemaker
+
+
+
+
+
+
+
